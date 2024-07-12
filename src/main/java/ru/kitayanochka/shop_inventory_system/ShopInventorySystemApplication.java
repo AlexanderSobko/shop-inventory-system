ff2aa48b00e0efb816adc7aea677949f6ed7d473
AlexanderSobko <Mr.AlexanderSobko@yandex.ru>
2024-07-12 12:41:26 +0800

    init SIS-4

---
+++ src/main/java/ru/kitayanochka/shop_inventory_system/ShopInventorySystemApplication.java
@@ -0,0 +1,13 @@
+package ru.kitayanochka.shop_inventory_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopInventorySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopInventorySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kitayanochka/shop_inventory_system/ShopInventorySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kitayanochka.shop_inventory_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopInventorySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
